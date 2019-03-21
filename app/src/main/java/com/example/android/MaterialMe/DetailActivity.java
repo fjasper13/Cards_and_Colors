8fbb8fecc84c82ed246c1a2c418cd3c41a4566b5
fjasper13 <fulgentiusjasper@gmail.com>
2019-03-21 17:07:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/android/MaterialMe/DetailActivity.java
@@ -0,0 +1,24 @@
+package com.example.android.MaterialMe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        TextView sportTitle = findViewById(R.id.titleDetail);
+        ImageView sportsImage = findViewById(R.id.sportsImageDetail);
+
+        sportTitle.setText(getIntent().getStringExtra("title"));
+
+        Glide.with(this).load(getIntent().getIntExtra("image_source",0)).into(sportsImage);
+    }
+}
